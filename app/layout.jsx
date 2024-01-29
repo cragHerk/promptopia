8668c01d973ddf3d979ba40e9953b312aed13b3f
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-01-29 18:24:59 +0100

    1

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import Nav from "@/components/Nav";
+import Provider from "@/components/Provider";
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from "@/components/Feed";
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        promptopia is free opensource tool for modern word discover to create
+        creative AI prompts
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
